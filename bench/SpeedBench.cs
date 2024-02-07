be958a3e830719dc3dc98308f8aff461e0a0c9bb
zenuas@gmail.com <zenuas@gmail.com>
2024-02-07 23:26:40 +0900

    ベンチマークテンプレート追加

---
+++ bench/SpeedBench.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Globalization;
+
+namespace Mina.Benchmark;
+
+public class SpeedBench
+{
+    [Benchmark]
+    public void Bench1()
+    {
+        object o = "2000/01/01";
+        DateTime n;
+
+        n = (DateTime)Convert.ChangeType(o, typeof(DateTime), CultureInfo.InvariantCulture);
+    }
+
+    [Benchmark]
+    public void Bench2()
+    {
+        object o = "2000/01/01";
+        DateTime n;
+
+        n = DateTime.Parse(o.ToString()!, CultureInfo.InvariantCulture);
+    }
+
+    [Benchmark]
+    public void Bench3()
+    {
+        object o = "2000/01/01";
+        DateTime n;
+
+        DateTime.TryParse(o.ToString()!, CultureInfo.InvariantCulture, out n);
+    }
+}
