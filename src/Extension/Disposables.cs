e450b49ac5b13a33d0f7751744c7f2deb6fb0a39
zenuas@gmail.com <zenuas@gmail.com>
2024-02-15 00:33:21 +0900

    Disposablesに対応

---
+++ src/Extension/Disposables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mina.Extension;
+
+public static class Disposables
+{
+    public static void Using<T>(this T self, Action<T> f) where T : IDisposable
+    {
+        using var x = self;
+        f(x);
+    }
+
+    public static R Using<T, R>(this T self, Func<T, R> f) where T : IDisposable
+    {
+        using var x = self;
+        return f(x);
+    }
+}
