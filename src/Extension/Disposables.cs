d1cc2d992ffc23d306a533ef6a5b6fd62305c913
zenuas@gmail.com <zenuas@gmail.com>
2024-02-18 21:40:03 +0900

    UsingDefer対応 Using、UsingDeferのテストケース追加

--- src/Extension/Disposables.cs
+++ src/Extension/Disposables.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Mina.Extension;
 
@@ -14,5 +16,40 @@ public static class Disposables
     {
         using var x = self;
         return f(x);
+    }
+
+    public static IEnumerable<R> UsingDefer<T, R>(this T self, Func<T, IEnumerable<R>> f) where T : IDisposable
+    {
+        return new AutoDisposableAtLastEnumerable<R>(f(self), self.Dispose);
+    }
+
+    public class AutoDisposableAtLastEnumerable<T>(IEnumerable<T> BaseEnumerable, Action? DeferDispose = null) : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator() => new AutoDisposableAtLastEnumerator<T>(BaseEnumerable.GetEnumerator(), DeferDispose);
+
+        IEnumerator IEnumerable.GetEnumerator() => new AutoDisposableAtLastEnumerator<T>(BaseEnumerable.GetEnumerator(), DeferDispose);
+    }
+
+    public class AutoDisposableAtLastEnumerator<T>(IEnumerator<T> BaseEnumerator, Action? DeferDispose = null) : IEnumerator<T>
+    {
+        public T Current => BaseEnumerator.Current;
+
+        object IEnumerator.Current => BaseEnumerator.Current!;
+
+        public void Dispose()
+        {
+            BaseEnumerator.Dispose();
+            if (DeferDispose is { }) DeferDispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public bool MoveNext()
+        {
+            var b = BaseEnumerator.MoveNext();
+            if (!b) Dispose();
+            return b;
+        }
+
+        public void Reset() => BaseEnumerator.Reset();
     }
 }
---
+++ test/DisposablesTest.cs
@@ -0,0 +1,74 @@
+﻿using Mina.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Mina.Test;
+
+public class DisposablesTest
+{
+    public class DisposeTest(string Name, List<string> Buffer) : IDisposable
+    {
+        public void Dispose()
+        {
+            Buffer.Add($"{Name} Dispose");
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    [Fact]
+    public void UsingTest()
+    {
+        var buffer = new List<string>();
+        Assert.Equal(buffer.Join(", "), "");
+
+        if (true)
+        {
+            using var a = new DisposeTest("a", buffer);
+            using var b = new DisposeTest("b", buffer);
+            Assert.Equal(buffer.Join(", "), "");
+        }
+        Assert.Equal(buffer.Join(", "), "b Dispose, a Dispose");
+
+        new DisposeTest("x", buffer).Using(x => buffer.Add("dummy"));
+        Assert.Equal(buffer.Join(", "), "b Dispose, a Dispose, dummy, x Dispose");
+
+        var n = new DisposeTest("y", buffer).Using(y =>
+        {
+            buffer.Add("dummy2");
+            Assert.Equal(buffer.Join(", "), "b Dispose, a Dispose, dummy, x Dispose, dummy2");
+            return 123;
+        });
+        Assert.Equal(n, 123);
+        Assert.Equal(buffer.Join(", "), "b Dispose, a Dispose, dummy, x Dispose, dummy2, y Dispose");
+    }
+
+    [Fact]
+    public void UsingDeferTest()
+    {
+        var buffer = new List<string>();
+        Assert.Equal(buffer.Join(", "), "");
+
+        var xs = new DisposeTest("xs", buffer)
+            .UsingDefer(x => Lists.Sequence(0))
+            .Take(3);
+
+        var e = xs.GetEnumerator();
+        Assert.Equal(buffer.Join(", "), "");
+        Assert.Equal(e.MoveNext(), true);
+        Assert.Equal(e.Current, 0);
+        Assert.Equal(buffer.Join(", "), "");
+
+        Assert.Equal(e.MoveNext(), true);
+        Assert.Equal(e.Current, 1);
+        Assert.Equal(buffer.Join(", "), "");
+
+        Assert.Equal(e.MoveNext(), true);
+        Assert.Equal(e.Current, 2);
+        Assert.Equal(buffer.Join(", "), "");
+
+        Assert.Equal(e.MoveNext(), false);
+        Assert.Equal(buffer.Join(", "), "xs Dispose");
+    }
+}
