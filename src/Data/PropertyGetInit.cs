d347bf31cb70b37f17e59ffaa6f403f5f54a3c61
zenuas@gmail.com <zenuas@gmail.com>
2024-06-21 18:52:25 +0900

    関節参照のためのプロパティ実装クラスを追加

---
+++ src/Data/PropertyGetInit.cs
@@ -0,0 +1,6 @@
+﻿namespace Mina.Data;
+
+public class PropertyGetInit<T>
+{
+    public required T Value { get; init; }
+}
---
+++ src/Data/PropertyGetSet.cs
@@ -0,0 +1,6 @@
+﻿namespace Mina.Data;
+
+public class PropertyGetSet<T>
+{
+    public required T Value { get; set; }
+}
