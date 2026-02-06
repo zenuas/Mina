992faaa6cbc001b342de3d619cb6ce6555ad1648
zenuas@gmail.com <zenuas@gmail.com>
2026-02-06 23:27:51 +0900

    UTF16WithBOMを移植

---
+++ src/Text/UTF16WithBOM.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Mina.Text;
+
+public class UTF16WithBOM : UnicodeEncoding
+{
+    public static readonly UTF16WithBOM UTF16_BEWithBOM = new(true);
+    public static readonly UTF16WithBOM UTF16_LEWithBOM = new(false);
+
+    public UTF16WithBOM(bool bigEndian)
+        : base(bigEndian, true)
+    {
+    }
+
+    public UTF16WithBOM(bool bigEndian, bool throwOnInvalidByte)
+        : base(bigEndian, true, throwOnInvalidByte)
+    {
+    }
+
+    public override int GetByteCount(char[] chars, int index, int count) => base.GetByteCount(chars, index, count) + 2;
+
+    public override int GetByteCount(string s) => base.GetByteCount(s) + 2;
+
+    public override unsafe int GetByteCount(char* chars, int count) => chars is null ? 2 : base.GetByteCount(chars, count) + 2;
+
+    public override int GetBytes(string s, int charIndex, int charCount, byte[] bytes, int byteIndex)
+    {
+        if (bytes.Length <= byteIndex) return 0;
+        var bom = GetPreamble();
+        bytes[byteIndex++] = bom[0];
+        if (bytes.Length <= byteIndex) return 1;
+        bytes[byteIndex++] = bom[1];
+
+        return base.GetBytes(s, charIndex, charCount, bytes, byteIndex) + 2;
+    }
+
+    public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+    {
+        if (bytes.Length <= byteIndex) return 0;
+        var bom = GetPreamble();
+        bytes[byteIndex++] = bom[0];
+        if (bytes.Length <= byteIndex) return 1;
+        bytes[byteIndex++] = bom[1];
+
+        return base.GetBytes(chars, charIndex, charCount, bytes, byteIndex) + 2;
+    }
+
+    public override unsafe int GetBytes(char* chars, int charCount, byte* bytes, int byteCount)
+    {
+        if (byteCount <= 0) return 0;
+        var bom = GetPreamble();
+        bytes[0] = bom[0];
+        if (byteCount <= 1) return 1;
+        bytes[1] = bom[1];
+
+        return chars is null ? 2 : base.GetBytes(chars, charCount, bytes + 2, byteCount - 2) + 2;
+    }
+}
---
+++ test/UTF16WithBOMTest.cs
@@ -0,0 +1,202 @@
+﻿using Mina.Text;
+using System;
+using Xunit;
+
+namespace Mina.Test;
+
+public class UTF16WithBOMTest
+{
+    [Fact]
+    public void GetBytesBETest()
+    {
+        var enc = UTF16WithBOM.UTF16_BEWithBOM;
+
+        Assert.Equal(enc.GetBytes(""), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes("abc"), [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        Assert.Equal(enc.GetBytes("", 0, 0), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes("abc", 0, 0), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes("abc", 0, 3), [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        char[] empty = [];
+        char[] abc = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetBytes(empty), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc), [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        Assert.Equal(enc.GetBytes(empty, 0, 0), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc, 0, 0), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc, 0, 3), [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        Span<byte> buffer = new byte[100];
+        ReadOnlySpan<char> empty2 = new(empty);
+        ReadOnlySpan<char> abc2 = new(abc);
+        Assert.Equal(enc.GetBytes(empty2, buffer), 2);
+        Assert.Equal(buffer[0..2].ToArray(), [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc2, buffer), 8);
+        Assert.Equal(buffer[0..8].ToArray(), [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        var bytes = new byte[100];
+        Assert.Equal(enc.GetBytes(empty, 0, 0, bytes, 0), 2);
+        Assert.Equal(bytes[0..2], [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc, 0, 0, bytes, 0), 2);
+        Assert.Equal(bytes[0..2], [0xFE, 0xFF]);
+        Assert.Equal(enc.GetBytes(abc, 0, 3, bytes, 0), 8);
+        Assert.Equal(bytes[0..8], [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+
+        unsafe
+        {
+            var bytes2 = new byte[100];
+            fixed (byte* bytes3 = bytes2)
+            fixed (char* empty3 = empty)
+            {
+                Assert.Equal(enc.GetBytes(empty3, 0, bytes3, 100), 2);
+                Assert.Equal(bytes2[0..2], [0xFE, 0xFF]);
+            }
+            fixed (byte* bytes3 = bytes2)
+            fixed (char* abc3 = abc2)
+            {
+                Assert.Equal(enc.GetBytes(abc3, 0, bytes3, 100), 2);
+                Assert.Equal(bytes2[0..2], [0xFE, 0xFF]);
+                Assert.Equal(enc.GetBytes(abc3, 3, bytes3, 100), 8);
+                Assert.Equal(bytes2[0..8], [0xFE, 0xFF, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63]);
+            }
+        }
+    }
+
+    [Fact]
+    public void GetBytesLETest()
+    {
+        var enc = UTF16WithBOM.UTF16_LEWithBOM;
+
+        Assert.Equal(enc.GetBytes(""), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes("abc"), [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        Assert.Equal(enc.GetBytes("", 0, 0), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes("abc", 0, 0), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes("abc", 0, 3), [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        char[] empty = [];
+        char[] abc = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetBytes(empty), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc), [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        Assert.Equal(enc.GetBytes(empty, 0, 0), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc, 0, 0), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc, 0, 3), [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        Span<byte> buffer = new byte[100];
+        ReadOnlySpan<char> empty2 = new(empty);
+        ReadOnlySpan<char> abc2 = new(abc);
+        Assert.Equal(enc.GetBytes(empty2, buffer), 2);
+        Assert.Equal(buffer[0..2].ToArray(), [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc2, buffer), 8);
+        Assert.Equal(buffer[0..8].ToArray(), [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        var bytes = new byte[100];
+        Assert.Equal(enc.GetBytes(empty, 0, 0, bytes, 0), 2);
+        Assert.Equal(bytes[0..2], [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc, 0, 0, bytes, 0), 2);
+        Assert.Equal(bytes[0..2], [0xFF, 0xFE]);
+        Assert.Equal(enc.GetBytes(abc, 0, 3, bytes, 0), 8);
+        Assert.Equal(bytes[0..8], [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+
+        unsafe
+        {
+            var bytes2 = new byte[100];
+            fixed (byte* bytes3 = bytes2)
+            fixed (char* empty3 = empty)
+            {
+                Assert.Equal(enc.GetBytes(empty3, 0, bytes3, 100), 2);
+                Assert.Equal(bytes2[0..2], [0xFF, 0xFE]);
+            }
+            fixed (byte* bytes3 = bytes2)
+            fixed (char* abc3 = abc2)
+            {
+                Assert.Equal(enc.GetBytes(abc3, 0, bytes3, 100), 2);
+                Assert.Equal(bytes2[0..2], [0xFF, 0xFE]);
+                Assert.Equal(enc.GetBytes(abc3, 3, bytes3, 100), 8);
+                Assert.Equal(bytes2[0..8], [0xFF, 0xFE, 0x61, 0x00, 0x62, 0x00, 0x63, 0x00]);
+            }
+        }
+    }
+
+    [Fact]
+    public void GetByteCountBETest()
+    {
+        var enc = UTF16WithBOM.UTF16_BEWithBOM;
+
+        Assert.Equal(enc.GetByteCount(""), 2);
+        Assert.Equal(enc.GetByteCount("abc"), 8);
+
+        Assert.Equal(enc.GetByteCount("", 0, 0), 2);
+        Assert.Equal(enc.GetByteCount("abc", 0, 0), 2);
+        Assert.Equal(enc.GetByteCount("abc", 0, 3), 8);
+
+        ReadOnlySpan<char> empty = [];
+        ReadOnlySpan<char> abc = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetByteCount(empty), 2);
+        Assert.Equal(enc.GetByteCount(abc), 8);
+
+        char[] empty2 = [];
+        char[] abc2 = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetByteCount(empty2), 2);
+        Assert.Equal(enc.GetByteCount(abc2), 8);
+
+        Assert.Equal(enc.GetByteCount(empty2, 0, 0), 2);
+        Assert.Equal(enc.GetByteCount(abc2, 0, 0), 2);
+        Assert.Equal(enc.GetByteCount(abc2, 0, 3), 8);
+
+        unsafe
+        {
+            fixed (char* empty3 = empty2)
+            {
+                Assert.Equal(enc.GetByteCount(empty3, 0), 2);
+            }
+            fixed (char* abc3 = abc2)
+            {
+                Assert.Equal(enc.GetByteCount(abc3, 0), 2);
+                Assert.Equal(enc.GetByteCount(abc3, 3), 8);
+            }
+        }
+    }
+
+    [Fact]
+    public void GetByteCountLETest()
+    {
+        var enc = UTF16WithBOM.UTF16_LEWithBOM;
+
+        Assert.Equal(enc.GetByteCount(""), 2);
+        Assert.Equal(enc.GetByteCount("abc"), 8);
+
+        Assert.Equal(enc.GetByteCount("", 0, 0), 2);
+        Assert.Equal(enc.GetByteCount("abc", 0, 0), 2);
+        Assert.Equal(enc.GetByteCount("abc", 0, 3), 8);
+
+        ReadOnlySpan<char> empty = [];
+        ReadOnlySpan<char> abc = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetByteCount(empty), 2);
+        Assert.Equal(enc.GetByteCount(abc), 8);
+
+        char[] empty2 = [];
+        char[] abc2 = ['a', 'b', 'c'];
+        Assert.Equal(enc.GetByteCount(empty2), 2);
+        Assert.Equal(enc.GetByteCount(abc2), 8);
+
+        Assert.Equal(enc.GetByteCount(empty2, 0, 0), 2);
+        Assert.Equal(enc.GetByteCount(abc2, 0, 0), 2);
+        Assert.Equal(enc.GetByteCount(abc2, 0, 3), 8);
+
+        unsafe
+        {
+            fixed (char* empty3 = empty2)
+            {
+                Assert.Equal(enc.GetByteCount(empty3, 0), 2);
+            }
+            fixed (char* abc3 = abc2)
+            {
+                Assert.Equal(enc.GetByteCount(abc3, 0), 2);
+                Assert.Equal(enc.GetByteCount(abc3, 3), 8);
+            }
+        }
+    }
+}
