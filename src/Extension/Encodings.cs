db1240adff91f486cc339872979f6bf0c6c1c9bd
zenuas@gmail.com <zenuas@gmail.com>
2026-02-15 13:31:07 +0900

    GetEncodingOrNull、GetEncodingOrUtf8追加

---
+++ src/Extension/Encodings.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Mina.Extension;
+
+public static class Encodings
+{
+    public static Encoding? GetEncodingOrNull(int codepage) => Objects.TryOrDefault(() => Encoding.GetEncoding(codepage));
+    public static Encoding? GetEncodingOrNull(int codepage, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => Objects.TryOrDefault(() => Encoding.GetEncoding(codepage, encoderFallback, decoderFallback));
+    public static Encoding? GetEncodingOrNull(string name) => Objects.TryOrDefault(() => Encoding.GetEncoding(name));
+    public static Encoding? GetEncodingOrNull(string name, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => Objects.TryOrDefault(() => Encoding.GetEncoding(name, encoderFallback, decoderFallback));
+
+    public static Encoding GetEncodingOrUtf8(int codepage) => GetEncodingOrNull(codepage) ?? Encoding.UTF8;
+    public static Encoding GetEncodingOrUtf8(int codepage, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => GetEncodingOrNull(codepage, encoderFallback, decoderFallback) ?? Encoding.GetEncoding(65001, encoderFallback, decoderFallback);
+    public static Encoding GetEncodingOrUtf8(string name) => GetEncodingOrNull(name) ?? Encoding.UTF8;
+    public static Encoding GetEncodingOrUtf8(string name, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => GetEncodingOrNull(name, encoderFallback, decoderFallback) ?? Encoding.GetEncoding(65001, encoderFallback, decoderFallback);
+}
---
+++ test/EncodingsTest.cs
@@ -0,0 +1,41 @@
+﻿using Mina.Extension;
+using System;
+using System.Text;
+using Xunit;
+
+namespace Mina.Test;
+
+public class EncodingsTest
+{
+    [Fact]
+    public void GetEncodingTest()
+    {
+        Assert.Throws<ArgumentException>(() => Encoding.GetEncoding(""));
+        Assert.Throws<ArgumentException>(() => Encoding.GetEncoding("xxx_not_found_xxx"));
+        Assert.Throws<NotSupportedException>(() => Encoding.GetEncoding(65535));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Encoding.GetEncoding(-1));
+        Assert.Equal(Encoding.GetEncoding(1200), Encoding.Unicode);
+    }
+
+    [Fact]
+    public void GetEncodingOrNullTest()
+    {
+        Assert.Equal(Encodings.GetEncodingOrNull(""), null);
+        Assert.Equal(Encodings.GetEncodingOrNull("xxx_not_found_xxx"), null);
+        Assert.Equal(Encodings.GetEncodingOrNull(65535), null);
+        Assert.Equal(Encodings.GetEncodingOrNull(-1), null);
+        Assert.Equal(Encodings.GetEncodingOrNull(1200), Encoding.Unicode);
+    }
+
+    [Fact]
+    public void GetEncodingOrUtf8Test()
+    {
+        var utf8 = Encoding.UTF8;
+
+        Assert.Equal(Encodings.GetEncodingOrUtf8(""), utf8);
+        Assert.Equal(Encodings.GetEncodingOrUtf8("xxx_not_found_xxx"), utf8);
+        Assert.Equal(Encodings.GetEncodingOrUtf8(65535), utf8);
+        Assert.Equal(Encodings.GetEncodingOrUtf8(-1), utf8);
+        Assert.Equal(Encodings.GetEncodingOrUtf8(1200), Encoding.Unicode);
+    }
+}
