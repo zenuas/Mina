79c2517d86b56098e6dbc851f751f0d7e8361f50
zenuas@gmail.com <zenuas@gmail.com>
2026-02-17 20:44:54 +0900

    GetBytes、GetStringのnull実装

--- src/Extension/Encodings.cs
+++ src/Extension/Encodings.cs
@@ -13,4 +13,11 @@ public static class Encodings
     public static Encoding GetEncodingOrUtf8(int codepage, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => GetEncodingOrNull(codepage, encoderFallback, decoderFallback) ?? Encoding.GetEncoding(65001, encoderFallback, decoderFallback);
     public static Encoding GetEncodingOrUtf8(string name) => GetEncodingOrNull(name) ?? Encoding.UTF8;
     public static Encoding GetEncodingOrUtf8(string name, EncoderFallback encoderFallback, DecoderFallback decoderFallback) => GetEncodingOrNull(name, encoderFallback, decoderFallback) ?? Encoding.GetEncoding(65001, encoderFallback, decoderFallback);
+
+    public static Encoding ToExceptionFallbackEncoding(this Encoding self) => self.EncoderFallback == EncoderFallback.ExceptionFallback && self.DecoderFallback == DecoderFallback.ExceptionFallback ? self : Encoding.GetEncoding(self.CodePage, EncoderFallback.ExceptionFallback, DecoderFallback.ExceptionFallback);
+
+    public static int? GetByteCountOrNull(this Encoding self, string s) => Objects.TryOrDefault(() => self.ToExceptionFallbackEncoding().GetByteCount(s).To(x => x > 0 || s == "" ? (int?)x : null));
+    public static byte[]? GetBytesOrNull(this Encoding self, string s) => Objects.TryOrDefault(() => self.ToExceptionFallbackEncoding().GetBytes(s));
+    public static int? GetCharCountOrNull(this Encoding self, byte[] bytes) => Objects.TryOrDefault(() => self.ToExceptionFallbackEncoding().GetCharCount(bytes).To(x => x > 0 || bytes.Length == 0 ? (int?)x : null));
+    public static string? GetStringOrNull(this Encoding self, byte[] bytes) => Objects.TryOrDefault(() => self.ToExceptionFallbackEncoding().GetString(bytes));
 }
--- test/EncodingsTest.cs
+++ test/EncodingsTest.cs
@@ -38,4 +38,64 @@ public class EncodingsTest
         Assert.Equal(Encodings.GetEncodingOrUtf8(-1), utf8);
         Assert.Equal(Encodings.GetEncodingOrUtf8(1200), Encoding.Unicode);
     }
+
+    [Fact]
+    public void GetByteCountOrNullTest()
+    {
+        var encoding = Encoding.ASCII;
+        Assert.Equal(encoding.GetByteCountOrNull(""), 0);
+        Assert.Equal(encoding.GetByteCountOrNull("abc"), 3);
+        Assert.Equal(encoding.GetByteCountOrNull("abcあいう"), null);
+        Assert.Equal(encoding.GetByteCountOrNull("🍣"), null);
+        Assert.Equal(encoding.GetByteCountOrNull("abcあいう🍣"), null);
+
+        Assert.Equal(encoding.GetByteCount(""), 0);
+        Assert.Equal(encoding.GetByteCount("abc"), 3);
+        Assert.Equal(encoding.GetByteCount("abcあいう"), 6);
+        Assert.Equal(encoding.GetByteCount("🍣"), 2);
+        Assert.Equal(encoding.GetByteCount("abcあいう🍣"), 8);
+    }
+
+    [Fact]
+    public void GetBytesOrNullTest()
+    {
+        var encoding = Encoding.ASCII;
+        Assert.Equal(encoding.GetBytesOrNull(""), new byte[] { });
+        Assert.Equal(encoding.GetBytesOrNull("abc"), new byte[] { 97, 98, 99 });
+        Assert.Equal(encoding.GetBytesOrNull("abcあいう"), null);
+        Assert.Equal(encoding.GetBytesOrNull("🍣"), null);
+        Assert.Equal(encoding.GetBytesOrNull("abcあいう🍣"), null);
+
+        Assert.Equal(encoding.GetBytes(""), new byte[] { });
+        Assert.Equal(encoding.GetBytes("abc"), new byte[] { 97, 98, 99 });
+        Assert.Equal(encoding.GetBytes("abcあいう"), new byte[] { 97, 98, 99, 63, 63, 63 });
+        Assert.Equal(encoding.GetBytes("🍣"), new byte[] { 63, 63 });
+        Assert.Equal(encoding.GetBytes("abcあいう🍣"), new byte[] { 97, 98, 99, 63, 63, 63, 63, 63 });
+    }
+
+    [Fact]
+    public void GetCharCountOrNullTest()
+    {
+        var encoding = Encoding.ASCII;
+        Assert.Equal(encoding.GetCharCountOrNull([]), 0);
+        Assert.Equal(encoding.GetCharCountOrNull([97, 98, 99]), 3);
+        Assert.Equal(encoding.GetCharCountOrNull([97, 98, 99, 255]), null);
+
+        Assert.Equal(encoding.GetCharCount([]), 0);
+        Assert.Equal(encoding.GetCharCount([97, 98, 99]), 3);
+        Assert.Equal(encoding.GetCharCount([97, 98, 99, 255]), 4);
+    }
+
+    [Fact]
+    public void GetStringOrNullTest()
+    {
+        var encoding = Encoding.ASCII;
+        Assert.Equal(encoding.GetStringOrNull([]), "");
+        Assert.Equal(encoding.GetStringOrNull([97, 98, 99]), "abc");
+        Assert.Equal(encoding.GetStringOrNull([07, 98, 99, 255]), null);
+
+        Assert.Equal(encoding.GetString([]), "");
+        Assert.Equal(encoding.GetString([97, 98, 99]), "abc");
+        Assert.Equal(encoding.GetString([97, 98, 99, 255]), "abc?");
+    }
 }
