47f59a239d0b036c6a816d3dc4c0f3283796723f
zenuas@gmail.com <zenuas@gmail.com>
2026-08-26 13:55:42 +0900

    IndentWriter追加

---
+++ src/Data/IndentWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Mina.Data;
+
+public class IndentWriter : IDisposable
+{
+    public required Stream BaseStream { get; init; }
+    public Encoding Encoding { get; init; } = Encoding.UTF8;
+    public string IndentChars { get; init; } = "\t";
+    public string LineBreakChars { get; init; } = "\n";
+    public int Indent { get; set; } = 0;
+    public bool IsNewLine { get; protected set; } = true;
+
+    public void Write(char c)
+    {
+        if (IsNewLine && Indent > 0) WriteIndent(BaseStream, Indent, IndentChars, Encoding);
+        IsNewLine = false;
+
+        Span<byte> buffer = stackalloc byte[Encoding.GetMaxByteCount(1)];
+        var count = Encoding.GetBytes([c], buffer);
+        BaseStream.Write(buffer[..count]);
+    }
+
+    public void Write(string s)
+    {
+        if (IsNewLine && Indent > 0) WriteIndent(BaseStream, Indent, IndentChars, Encoding);
+        IsNewLine = false;
+
+        Span<byte> buffer = stackalloc byte[Encoding.GetMaxByteCount(s.Length)];
+        var count = Encoding.GetBytes(s, buffer);
+        BaseStream.Write(buffer[..count]);
+    }
+
+    public void WriteLine()
+    {
+        Write(LineBreakChars);
+        IsNewLine = true;
+    }
+
+    public void WriteLine(string s)
+    {
+        Write(s);
+        WriteLine();
+    }
+
+    public static void WriteIndent(Stream stream, int indent, string indent_chars, Encoding encoding)
+    {
+        Span<byte> buffer = stackalloc byte[encoding.GetMaxByteCount(indent_chars.Length)];
+        var count = encoding.GetBytes(indent_chars, buffer);
+        var chars = buffer[..count];
+        for (var i = 0; i < indent; i++) stream.Write(chars);
+    }
+
+    public void Flush() => BaseStream.Flush();
+
+    public void Close() => Dispose();
+
+    public void Dispose()
+    {
+        BaseStream.Close();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ test/IndentWriterTest.cs
@@ -0,0 +1,97 @@
+﻿using Mina.Data;
+using System.IO;
+using Xunit;
+
+namespace Mina.Test;
+
+public class IndentWriterTest
+{
+    [Fact]
+    public void NullTest()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), []);
+    }
+
+    [Fact]
+    public void WriteCharTest()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.Write('a');
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), [(byte)'a']);
+    }
+
+    [Fact]
+    public void WriteCharWithIndentTest()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.Write('a');
+        writer.WriteLine();
+        writer.Indent += 1;
+        writer.Write('b');
+        writer.WriteLine();
+        writer.Indent += 1;
+        writer.Write('c');
+        writer.WriteLine();
+        writer.Indent -= 2;
+        writer.Write('d');
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), [(byte)'a', (byte)'\n', (byte)'\t', (byte)'b', (byte)'\n', (byte)'\t', (byte)'\t', (byte)'c', (byte)'\n', (byte)'d']);
+    }
+
+    [Fact]
+    public void WriteStringTest()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.Write("abc");
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), [(byte)'a', (byte)'b', (byte)'c']);
+    }
+
+    [Fact]
+    public void WriteStringWithIndentTest()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.WriteLine("abc");
+        writer.Indent += 1;
+        writer.WriteLine("bcd");
+        writer.Indent += 1;
+        writer.WriteLine("cde");
+        writer.Indent -= 2;
+        writer.WriteLine("def");
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), [(byte)'a', (byte)'b', (byte)'c', (byte)'\n', (byte)'\t', (byte)'b', (byte)'c', (byte)'d', (byte)'\n', (byte)'\t', (byte)'\t', (byte)'c', (byte)'d', (byte)'e', (byte)'\n', (byte)'d', (byte)'e', (byte)'f', (byte)'\n']);
+    }
+
+    [Fact]
+    public void WriteStringWithIndentTest2()
+    {
+        using var mem = new MemoryStream();
+        using var writer = new IndentWriter() { BaseStream = mem };
+        writer.Write("a");
+        writer.WriteLine("bc");
+        writer.Indent += 1;
+        writer.Write("bc");
+        writer.WriteLine("d");
+        writer.Indent += 1;
+        writer.Write("cde");
+        writer.WriteLine();
+        writer.Indent -= 2;
+        writer.WriteLine("def");
+        writer.Close();
+
+        Assert.Equal<byte[]>(mem.ToArray(), [(byte)'a', (byte)'b', (byte)'c', (byte)'\n', (byte)'\t', (byte)'b', (byte)'c', (byte)'d', (byte)'\n', (byte)'\t', (byte)'\t', (byte)'c', (byte)'d', (byte)'e', (byte)'\n', (byte)'d', (byte)'e', (byte)'f', (byte)'\n']);
+    }
+}
